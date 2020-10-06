f07dc1b961dcb436e7a32372637fefe6bce7d135
Felipe Cardoso <felipecrdos@gmail.com>
2020-10-06 05:17:03 -0300

    change boss path2D

--- scripts/boss/BossPath.gd
+++ scripts/boss/BossPath.gd
@@ -4,18 +4,24 @@ var index : int
 var speed : float
 var follow : PathFollow2D
 var level_data : Dictionary
+var screen_width : float
+var screen_height : float
 
 func _ready():
 	speed = 45
 	follow = $Follow
 	index = level_data["index"]
 	level_data = Global.game_data["Level"]
-
+	
+	screen_width = get_viewport_rect().size.x
+	screen_height = get_viewport_rect().size.y
+	position.x = screen_width/2
+	position.y = -screen_height/2
+	
 func _physics_process(delta):
 	follow.offset += speed * delta
 	
 	if level_data["boss"]["state"][index] == Global.FIGHTING:
 		position.y += 20 * delta
-		pass
 	
 	

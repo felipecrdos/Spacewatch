dc7b9c2955a992a90cf2452107a6079a2b591bb5
Felipe Cardoso <felipecrdos@gmail.com>
2020-10-06 20:46:53 -0300

    change boss image HUD

--- scripts/weapon/enemy/EAGun.gd
+++ scripts/weapon/enemy/EAGun.gd
@@ -1,10 +1,8 @@
 extends Weapon
-class_name ECircularGun
+class_name EAGun
 
 var can_shooting = false
 var active = false
-var angle = 0
-var time = 2
 
 func _ready():
 	get_barrels()
@@ -12,18 +10,12 @@ func _ready():
 
 func _physics_process(delta):
 	if active && can_shooting:
-		update_angle(delta)
 		shoot()
-
-func update_angle(delta):
-	angle += 2 * PI * delta/time
-	angle = wrapf(angle, -PI, PI)
-	$Barrel.rotation = angle
-
+		
 func on_start_shoot_timeout():
 	can_shooting = true
 	$StopShoot.start()
-	
+
 func on_stop_shoot_timeout():
-	can_shooting = false 
+	can_shooting = false
 	$StartShoot.start()
--- scripts/weapon/enemy/ECGun.gd
+++ scripts/weapon/enemy/ECGun.gd
@@ -1,5 +1,5 @@
 extends Weapon
-class_name EMachineGun
+class_name ECGun
 
 var can_shooting = false
 var active = false
@@ -11,11 +11,18 @@ func _ready():
 func _physics_process(delta):
 	if active && can_shooting:
 		shoot()
-		
+
+func rotation_barrels():
+	for barrel in barrels:
+		barrel.rotation += PI/12 # += 45
+
+	
 func on_start_shoot_timeout():
 	can_shooting = true
 	$StopShoot.start()
-
+	rotation_barrels()
+	
 func on_stop_shoot_timeout():
 	can_shooting = false
 	$StartShoot.start()
+	
